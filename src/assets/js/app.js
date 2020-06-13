de621dc9df0d10e2572530af1429bcd7290e55ad
jgj <hghjg>
2020-06-14 00:14:51 +0300

    feat: swiper

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -90,59 +90,21 @@
   
 
      /* ---------------------------------------------------------------------------
-     * Slick Slider init
+     * swiper Slider init
      * --------------------------------------------------------------------------- */
-  $('.responsive').slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    lazyLoad: 'ondemand',
-    autoplay: true,
-    autoplaySpeed: 4000,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          lazyLoad: 'ondemand',
-          infinite: true,
-          dots: true,
-          autoplay: true,
-          autoplaySpeed: 4000,
-        }
+    var swiper = new Swiper('.swiper-container', {
+      // Enable lazy loading
+      lazy: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
       },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          lazyLoad: 'ondemand',
-          dots: true,
-          autoplay: true,
-          autoplaySpeed: 4000,
-       
-        }
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
       },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          lazyLoad: 'ondemand',
-          dots: true,
-          autoplay: true,
-          autoplaySpeed: 4000,
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  });
 
+    });
 
      /* ---------------------------------------------------------------------------
      * quick menu events 
