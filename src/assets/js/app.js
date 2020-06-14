b648f9a6759328f240f0d2695296cdcf01c08636
jgj <hghjg>
2020-06-15 02:05:58 +0300

    feat: full page done

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -106,6 +106,22 @@
 
     });
 
+
+    var swiper = new Swiper('.swiper-group', {
+      slidesPerView: 6,
+      spaceBetween: 20,
+      slidesPerGroup: 6,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
      /* ---------------------------------------------------------------------------
      * quick menu events 
      * --------------------------------------------------------------------------- */
